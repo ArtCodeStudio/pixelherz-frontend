3a7ca3ba5ce6b27fb136d89c3ce96172995dd816
Daniel <dotonesimple@gmail.com>
2019-05-08 15:49:44 +0200

    added frame preview, frame position controls, heart shape, improved sliders and buttons

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -15,13 +15,18 @@ export class TestComponent implements OnInit {
   private frames: Frame[];
   private currentFrame: Frame;
 
+  private colorPalette: MatrixCell[];
+
   private red: number = 255;
   private green: number = 255;
   private blue: number = 255;
+  
+  private heartShape: boolean = false;
 
   constructor(private http: HttpClient) {
     this.frames = [];
     this.loadFrames();
+    this.colorPalette = Array(16);
   }
 
   ngOnInit() {
@@ -70,19 +75,52 @@ export class TestComponent implements OnInit {
     if(this.frames.length <= 1) return;
     let indexOf = this.frames.indexOf(this.currentFrame);
     this.frames.splice(indexOf, 1);
-    this.loadFrame(this.frames[Math.max(0, indexOf-1)]);
+    this.loadFrame(this.frames[Math.min(this.frames.length-1, indexOf)]);
     this.sendRequest();
   }
 
   addFrame() {
     let cells: MatrixCell[] = Array(64); 
     for(let i = 0; i < 64; i++) {
-      cells[i] = new MatrixCell(i, 0, 0, 0);
+      cells[i] = new MatrixCell(i, 
+        this.currentFrame.data[i].red, 
+        this.currentFrame.data[i].green,
+        this.currentFrame.data[i].blue
+        );
     }
     let frame: Frame = new Frame(100, cells);
     this.frames[this.frames.length] = frame;
     this.loadFrame(frame);
     this.sendRequest();
+  }
+
+  left() {
+    let indexOf: number = this.frames.indexOf(this.currentFrame);
+    this.frames[indexOf] = this.frames[indexOf-1];
+    this.frames[indexOf-1] = this.currentFrame;
+    this.sendRequest();
+  }
+
+  right() {
+    let indexOf: number = this.frames.indexOf(this.currentFrame);
+    this.frames[indexOf] = this.frames[indexOf+1];
+    this.frames[indexOf+1] = this.currentFrame;
+    this.sendRequest();
+  }
+
+  private heart: boolean[] = [
+    false,false,false,false,false,false,false,false,
+    false,true ,true ,false,false,true ,true ,false,
+    true ,true ,true ,true ,true ,true ,true ,true ,
+    true ,true ,true ,true ,true ,true ,true ,true ,
+    true ,true ,true ,true ,true ,true ,true ,true ,
+    false,true ,true ,true ,true ,true ,true ,false,
+    false,false,true ,true ,true ,true ,false,false,
+    false,false,false,true ,true ,false,false,false,
+  ];
+
+  isInHeart(cell: MatrixCell): boolean {
+    return this.heart[cell.position];
   }
 
   sendRequest() {
